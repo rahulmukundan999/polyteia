257cc8929aeaf9b3f703dd5e30ffe392e855cdb1
rahulmukundan <rahul.mukundan@panion.org>
2024-02-04 23:10:45 +0100

    added validation pipe and transformer

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const port = process.env.BACKEND_PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
 bootstrap();
--- backend/src/statistics/dto/query-statistics.ts
+++ backend/src/statistics/dto/query-statistics.ts
@@ -1,11 +1,14 @@
-import { IsDate, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsDate } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class QueryStatisticsDto {
   @IsNotEmpty()
   @IsDate()
+  @Transform(({ value }) => new Date(value))
   dateFrom: Date;
 
   @IsNotEmpty()
   @IsDate()
+  @Transform(({ value }) => new Date(value))
   dateTo: Date;
 }
